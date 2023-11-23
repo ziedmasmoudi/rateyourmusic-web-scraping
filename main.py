92ea6d82a3380f7104b19fa6163b423e13145034
Zied <zied.masmoudi2001@gmail.com>
2023-11-23 13:43:48 -0500

    implemented setting that allows for using a pre-saved http response to reduce number of requests made to server

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
+
+from_file = True
 
 URL = "https://rateyourmusic.com/genre/rock/"
 
@@ -6,6 +9,16 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 '
                   'Safari/537.36'
 }
-page = requests.get(URL, headers=headers)
+if not from_file:
+    page = requests.get(URL, headers=headers)
+    page_content = page.content
+    with open("response.txt", "wb") as f:
+        f.write(page.content)
+    print("From HTTP request")
+else:
+    page_content = open("response.txt", "rb")
+    print("From saved file")
 
-print(page.text)
+soup = BeautifulSoup(page_content, "html.parser")
+
+print(soup.prettify())
